a2c9e50639fe43761d290fc9faafac87d25c3f9e
Sushant <panditsushant11@gmail.com>
2025-05-02 16:53:16 +0530

    add loading code

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      Loading
+    <div className="flex h-full justify-center items-center">
+      <div
+        className=" animate-spin border-4 w-[5vmax] 
+        border-t-transparent border-b-transparent border-l-red-300 border-r-red-300
+    h-[5vmax] rounded-full border-red-300"
+      ></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
-Loading
+export default Loading;
+Loading;
