94ca3d904c61ef9602c4caf023c9ce2bfa9a84bb
Sushant <panditsushant11@gmail.com>
2025-05-02 14:39:10 +0530

    add origina image space or enhanced image space

--- client/src/components/ImagePreview.jsx
+++ client/src/components/ImagePreview.jsx
@@ -4,8 +4,23 @@ import React from 'react'
 
 const ImagePreview = () => {
   return (
-    <div>
-      ImpagePreview
+    // this is main div 
+    <div className='mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl'>
+      {/* this div is store the original image or local uplaoded image? */}
+      <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+        <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">Original Image</h2>
+        <img src="" alt="" className="w-full h-full object-cover" />
+      </div>
+
+
+
+      {/* this div is show the enhanced image of the original image  */}
+      <div
+      className="bg-white shadow-lg rounded-xl overflow-hidden"
+      >
+      <h2
+      className="text-xl font-semibold text-center bg-gray-800 text-white py-2"
+      >Enhanced Image</h2></div>
     </div>
   )
 }
