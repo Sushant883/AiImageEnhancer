cee0bf33f5c3cb0a74c1d5e3be79c9c4de6e73d8
Sushant <panditsushant11@gmail.com>
2025-05-02 12:18:53 +0530

    add input image option

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,11 +13,22 @@ const App = ()=> {
     <div className="flex flex-col items-center justify-center
     min-h-screen bg-gray-100 py-8 px-4">
 
-      Header 
+      <div className="text-center mb-8">
+        <h1 className="text-5xl font-bold text-gray-800 mb-2">AI Image Enhancer
+          {" "}
+        </h1>
+        <p className="text-lg text-gray-500">   Upload your Image and let AI enhance to in seconds !</p>
+      </div>
+
 
       <Home />
 
-      footer
+      <div className="text-lg text-gray-500 mt-6">
+      
+        This is the platform which is give 4k quality image
+      </div>
+
+      
     </div>
   );
 }
--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -1,13 +1,18 @@
 // this is going in the Home.jsx page as a component
 
-import React from 'react'
+import React from "react";
 
 const ImageUpload = () => {
   return (
-    <div>
-      ImageUpload
+    <div className="bg-white shodow-lg rounded-2xl p-6 w-full max-2xl">
+      <label
+        htmlFor="fileInput"
+        className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6"
+      >
+        <p>Click and drap to upload your image</p>
+      </label>
     </div>
-  )
-}
+  );
+};
 
-export default ImageUpload
+export default ImageUpload;
